7f5912605b9dcab9ab34ebfc0266626709d8ffd4
Taras Kozmovych <taraskom0404@gmail.com>
2022-08-22 23:31:09 +0300

    start

---
+++ src/fetchCountries.js
@@ -0,0 +1,13 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+
+export default function fetchCountries(name) {
+    const errorMassage = "Oops, there is no country with that name";
+    return fetch("https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages")
+        .then(response => {
+            if (!response.ok) {
+                throw Notify.failure(errorMassage);
+            }
+            return response.json();
+        })
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './css/styles.css';
-
+import fetchCountry from "./fetchCountries";
+import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+import fetchCountry from './fetchApi';
+import debounce from 'lodash.debounce';
 const DEBOUNCE_DELAY = 300;
+const refs = {
+    input: document.querySelector("#search-box"),
+    listCountry: document.querySelector(".country-list"),
+    infoCountry: document.querySelector(".country-info")
+};
+refs.input.addEventListener("input", debounce(inputChange, DEBOUNCE_DELAY));
+
